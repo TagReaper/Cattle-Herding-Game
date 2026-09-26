8ddf81d0cca30f5edbc1696347174656a5f2b7b7
TagReaper <coxlucas2022@gmaill.com>
2026-09-25 23:04:20 -0500

    Player Script Modifications

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -6,20 +6,23 @@ extends CharacterBody2D
 @export var growl_cooldown: float = 10
 @export var grown_duration: float = 3
 @export var bark_cooldown: float = 20
-@export var bark_severity: float
+@export var level_timer: float = 30
 
 # Node Variables
 @export var GrowlTimer: Timer
 @export var GrowlLeftNode: Node2D
 @export var GrowlRightNode: Node2D
 @export var BarkTimer: Timer
+@export var LevelTimer: Timer
 
 # Internal Variables
+const MOVEMENT_MULTIPLIER: float = 16
 var current_state: int
+var direction: Vector2 = Vector2.ZERO
+var friction: float = 1
 
 enum State {
 	IDLE,
-	WALK,
 	RUN,
 	GROWL,
 	BARK
@@ -29,28 +32,51 @@ func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	pass
+	
+	# State Check
+	if current_state != State.BARK:
+		direction.x = Input.get_axis("MoveLeft", "MoveRight")
+		direction.y = Input.get_axis("MoveUp", "MoveDown")
+		direction = direction.normalized()
+		
+		if current_state == State.GROWL:
+			direction /= 2
+		
+		if direction != Vector2.ZERO:
+			velocity.x = move_toward(velocity.x, speed * MOVEMENT_MULTIPLIER, acceleration * friction)
+			velocity.y = move_toward(velocity.y, speed * MOVEMENT_MULTIPLIER, acceleration * friction)
+			if current_state != State.GROWL:
+				current_state = State.RUN
+		else:
+			velocity.x = move_toward(velocity.x, 0, acceleration * friction)
+			velocity.y = move_toward(velocity.y, 0, acceleration * friction)
+			if current_state != State.IDLE:
+				current_state = State.RUN
 
 # Depends on sprites
 #func _flip_check() -> void:
 	#pass
 
-func _animation_check() -> void:
-	pass
+# Add when Sprites added
+#func _animation_check() -> void:
+	#pass
+
+# Gets the flee direction based on what state the player is in
+func _get_flee_direction() -> Vector2:
+	match current_state:
+		State.GROWL: # Run Perpendicular to the Growl Line
+			return Vector2.ZERO
+		State.BARK: # Run to the pen
+			return Vector2.ZERO
+		_:
+			return Vector2.ZERO
 
 # Makes Cows run away from the line in the center of the growl area
 func _growl() -> void:
-	pass
-
-# Gets the flee direction based on what state the player is in
-func _get_flee_direction() -> void:
-	match current_state:
-		State.IDLE, State.WALK, State.RUN: # Run away from the player
-			pass
-		State.GROWL: # Run Perpendicular to the Growl Line
-			pass
-		State.BARK: # Run to the pen
-			pass
+	if GrowlTimer.is_stopped():
+		print("GRRRR")
+		await get_tree().create_timer(grown_duration).timeout
+		GrowlTimer.start(growl_cooldown)
 
 # Makes the cows in the bark area run to the pen
 func _bark() -> void:
